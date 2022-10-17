5c68835edf881f5ce1d8aaf2872b87dbd45a72f2
Theresa Szoke <theresa.szoke@gmail.com>
2022-10-16 20:15:48 -0400

    updated mission to mars app html page

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import scraping
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def index():
